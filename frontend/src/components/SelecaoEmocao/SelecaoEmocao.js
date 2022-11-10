4519465bdcf0c18fa49e7cbf0aed271a72f9c93b
feelimelek <80131744+feelimelek@users.noreply.github.com>
2022-11-10 18:25:54 -0300

    infos passando entre o input, a selecao de emocao e o botao de enviar na pagina ajudar. Post feito, mas falta testar.

--- frontend/src/components/SelecaoEmocao/SelecaoEmocao.js
+++ frontend/src/components/SelecaoEmocao/SelecaoEmocao.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import styles from './SelecaoEmocao.module.css';
 
-const SelecaoEmocao = () => {
-
+const SelecaoEmocao = (props) => {
+  function handleChange(event) {
+    console.log(event.target.value);
+    props.handleSelectChange(event.target.value);
+  }
   return (
   <div className={styles.divSelecaoEmocao} data-testid="SelecaoEmocao">
-    <select className={styles.selectSelecaoEmocao} onChange="#">
+    <select className={styles.selectSelecaoEmocao} onChange={handleChange}>
       <option className={styles.option}>Selecione a emoção</option>
       <option className={styles.option}>Feliz</option>
       <option className={styles.option}>Triste</option>
--- frontend/src/views/PaginaAjudar/PaginaAjudar.js
+++ frontend/src/views/PaginaAjudar/PaginaAjudar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './PaginaAjudar.module.css';
 import LabelExperiencia from '../../components/LabelExperiencia/LabelExperiencia';
 import LabelQuandoVcEstava from '../../components/LabelQuandoVcEstava/LabelQuandoVcEstava';
@@ -6,16 +6,47 @@ import LabelOQTeAjudou from '../../components/LabelOQTeAjudou/LabelOQTeAjudou';
 import SelecaoEmocao from '../../components/SelecaoEmocao/SelecaoEmocao';
 import InputDica from '../../components/InputDica/InputDica';
 import BotaoEnviar from '../../components/BotaoEnviar/BotaoEnviar';
+import axios from 'axios';
 
 const PaginaAjudar = () => {
+  const [emocao, setEmocao] = useState(null);
+  const [dica, setDica] = useState(null);
+
+  function changeDica(dica) {
+    setDica(dica);
+    console.log(dica);
+  }
+
+  function changeEmocao(emocao) {
+    setEmocao(emocao);
+    // TODO remover prox linha
+    console.log(emocao);
+  }
+
+  function submitDica() {
+    //TODO remover prox linha
+    console.log("Submit Dica");
+    const axios = require('axios');
+    const urlPostDica = '/sugestao/';
+    axios.post(urlPostDica, {
+      id_usuario: 1,      
+      id_sugestao: 5,
+      sugestao: dica
+    }).then(function (response){
+      console.log(response);
+    }).catch(function (error) {
+      console.log(error);
+    })
+  }
+
   return (
     <div className={styles.divPaginaAjudar} data-testid="PaginaAjudar">
       <LabelExperiencia />
       <LabelQuandoVcEstava />
-      <SelecaoEmocao />
+      <SelecaoEmocao handleSelectChange={changeEmocao}/>
       <LabelOQTeAjudou />
-      <InputDica />
-      <BotaoEnviar />
+      <InputDica handleInputChange={changeDica} />
+      <BotaoEnviar handleButtonClick={submitDica}/>
     </div>
   );
 }
